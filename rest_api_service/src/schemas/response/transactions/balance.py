42e9eb8e10077d33ebb17ccdb1b309ee36d6798b
QdMiracle <qdmiracle21@gmail.com>
2025-02-03 18:00:31 +0300

    update response schema to conver Wei to ETH

--- rest_api_service/src/schemas/response/transactions/balance.py
+++ rest_api_service/src/schemas/response/transactions/balance.py
@@ -1,6 +1,12 @@
+from _decimal import Decimal
 from src.schemas.response.base import BaseResponse
 
 
 class BalanceChangeResponse(BaseResponse):
     address: str
-    balance_change: float
+    balance_change: str
+
+    @classmethod
+    def from_wei(cls, address: str, balance_change: float):
+        balance_in_eth = Decimal(balance_change) / Decimal(10**18)
+        return cls(address=address, balance_change=f"{balance_in_eth:.10f} ETH")
