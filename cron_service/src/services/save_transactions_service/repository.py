c24ec44eba022e1b981130fb4be50392e83539a7
QdMiracle <qdmiracle21@gmail.com>
2025-02-03 18:22:41 +0300

    move logger level to facade

--- cron_service/src/services/save_transactions_service/repository.py
+++ cron_service/src/services/save_transactions_service/repository.py
@@ -2,7 +2,6 @@ from src.database.repositories.transaction_repository import (
     TransactionRepository,
 )
 from src.schemas.response.transactions.base import TransactionBaseResponse
-from src.services.logging_service.logging_factory import Logger
 from src.services.save_transactions_service.abc_service import (
     AbstractSaveTransactionsService,
 )
@@ -10,17 +9,14 @@ from src.services.save_transactions_service.abc_service import (
 
 class RepositorySaveTransactionsService(AbstractSaveTransactionsService):
     def __init__(
-        self, transaction_repository: TransactionRepository, logger: Logger
+        self,
+        transaction_repository: TransactionRepository,
     ):
         self._transaction_repository = transaction_repository
-        self._logger = logger
 
     async def save_transactions(
         self, transactions: list[TransactionBaseResponse]
     ) -> None:
-        self._logger.info(
-            f"Saving transactions {len(transactions)} to the database."
-        )
         await self._transaction_repository.create_transactions(
             transactions=transactions
         )
